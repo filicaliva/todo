54e215912fe0acd5f40ab037077ba7f42b82929e
filicaliva <41651332+filicaliva@users.noreply.github.com>
2020-06-25 17:23:33 +0200

    add edit

--- src/features/list/Item/Item.jsx
+++ src/features/list/Item/Item.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 
 import bin from './symbol/bin.png'
+import edit from './symbol/edit.svg'
 
 import './checkbox.css'
 import styles from './Item.module.css'
 
-import { remove, setChecked } from '../listSlice'
+import { remove, setChecked, setEdit } from '../listSlice'
 
 export const Item = ({ id, value, checked }) => {
     const dispatch = useDispatch()
@@ -28,12 +29,21 @@ export const Item = ({ id, value, checked }) => {
                 </span>
 
             </div>
-            <img
-                src={bin}
-                alt="bin"
-                className={styles.item_bin}
-                onClick={() => dispatch(remove(id))}
-            />
+            <div className={styles.item__controls}>
+                <img
+                    src={edit}
+                    alt="correct"
+                    className={`${styles.cursor} ${styles.edit}`}
+                    onClick={() => dispatch(setEdit({id, value}))}
+                />
+                <img
+                    src={bin}
+                    alt="bin"
+                    className={`${styles.cursor} ${styles.remove}`}
+                    onClick={() => dispatch(remove(id))}
+                />
+
+            </div>
         </li>
 
     )
--- src/features/list/listSlice.js
+++ src/features/list/listSlice.js
@@ -4,18 +4,30 @@ export const listSlice = createSlice({
   name: 'list',
   initialState: {
     list: JSON.parse(window.localStorage.getItem('list')) || [],
-    item: window.localStorage.getItem('item') || ''
+    item: window.localStorage.getItem('item') || '',
+    id: window.localStorage.getItem('id') || ''
   },
   reducers: {
     add: (state, action) => {
-      if (state.item !== '' && action.payload === 'Enter') {
-        state.list.push({
-          id: Date.now(),
-          value: state.item,
-          checked: false
-        })
-        state.item = ''
+      if (action.payload === 'Enter') {
+        if (state.item !== '' && state.id === '') {
+          state.list.push({
+            id: Date.now(),
+            value: state.item,
+            checked: false
+          })
+          state.item = ''
+        } else {
+          state.list
+            .filter(item => +item.id === +state.id)
+            .map(item => item.value = state.item)
+          state.item = ''
+          state.id = ''
+          window.localStorage.setItem('id', state.id)
+        }
+
       }
+
       window.localStorage.setItem('list', JSON.stringify(state.list))
       window.localStorage.setItem('item', state.item)
     },
@@ -23,23 +35,30 @@ export const listSlice = createSlice({
       state.item = action.payload
       window.localStorage.setItem('item', state.item)
     },
-    remove: (state, action) => {      
+    remove: (state, action) => {
       state.list = state.list.filter(item => item.id !== action.payload)
       window.localStorage.setItem('list', JSON.stringify(state.list))
     },
     setChecked: (state, action) => {
       state.list
-      .filter(item => item.id === action.payload)
-      .map(item=> item.checked = !item.checked);
+        .filter(item => item.id === action.payload)
+        .map(item => item.checked = !item.checked);
       window.localStorage.setItem('list', JSON.stringify(state.list))
+    },
+    setEdit: (state, action) => {
+      state.item = action.payload.value
+      state.id = action.payload.id
+      window.localStorage.setItem('item', state.item)
+      window.localStorage.setItem('id', state.id)
     }
   },
 });
 
-export const { add, setItem, remove, setChecked } = listSlice.actions;
+
 
 
 export const selectItem = state => state.list.item;
 export const selectList = state => state.list.list;
 
+export const { add, setItem, remove, setChecked, setEdit } = listSlice.actions;
 export default listSlice.reducer;
